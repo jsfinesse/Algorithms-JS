09e45f74de55137e569a27d4ab432a3e3767d523
Prithu Singh <prithusingh0327@gmail.com>
2022-04-14 18:35:09 +0530

    Added Binary Search Alt

--- BinarySearch/BinarySearch.js
+++ BinarySearch/BinarySearch.js
@@ -9,3 +9,16 @@ function binarySearch(arr, elem) {
     }
     return arr[middle] === elem ? middle : -1;
 }
+
+function binarySearchAlt(arr, val) {
+    let left = 0,
+        right = arr.length - 1;
+    let mid;
+    while (left <= right) {
+        mid = Math.floor(left + (right - left) / 2);
+        if (arr[mid] === val) return mid;
+        else if (arr[mid] > val) right = mid - 1;
+        else left = mid + 1;
+    }
+    return -1;
+}
