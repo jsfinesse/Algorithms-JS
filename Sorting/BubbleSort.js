7ecc92a61bc23295f58355d8c065cda450cf1d7c
Prithu Singh <prithusingh0327@gmail.com>
2022-04-14 18:41:31 +0530

    Added optimised bubble sort

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -12,4 +12,22 @@ function bubbleSort(arr) {
     return arr;
 }
 
+function bubbleSortOptimised(arr) {
+    let noSwaps;
+    for (let i = arr.length; i > 0; --i) {
+        noSwaps = true;
+        for (let j = 0; j < i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                noSwaps = false;
+            }
+        }
+        if (noSwaps) break;
+    }
+    return arr;
+}
+
 console.log(bubbleSort([27, 12, 3, 69]));
