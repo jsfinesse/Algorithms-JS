c87d2fc44596d6eeec25cb97e50bf409724ed80c
Prithu Singh <prithusingh0327@gmail.com>
2022-04-14 18:37:39 +0530

    Added Bubble sort

---
+++ Sorting/BubbleSort.js
@@ -0,0 +1,15 @@
+function bubbleSort(arr) {
+    for (let i = arr.length; i > 0; --i) {
+        for (let j = 0; j < i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort([27, 12, 3, 69]));
